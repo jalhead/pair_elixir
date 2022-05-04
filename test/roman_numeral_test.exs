63d04856f7067219f2cbfecb75b4f5bc63bab585
jonathanhead <jonathan.head@currencycloud.com>
2022-05-04 16:45:40 +0100

    COLL-146 Change event types

--- test/roman_numeral_test.exs
+++ test/roman_numeral_test.exs
@@ -4,4 +4,8 @@ defmodule RomanNumeralTest do
   test "convert i returns 1" do
     assert RomanNumeral.convert("i") == {:ok, 1}
   end
+
+  test "convert ii returns 2" do
+    assert RomanNumeral.convert("ii") == {:ok, 2}
+  end
 end
